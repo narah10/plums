d4e248049215d39b6d92f71a467906ae1d7434f5
Lisa <lisacjheinhold@gmail.com>
2024-02-02 09:02:51 -0700

    created component hero base added colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'purple': '#8D3A87',
+        'gray': '#F8F8F8',
+        'dark-gray': '#908A8A',
+        'btn-purple': '#92369C',
+        'sch-purple': '#915797',
+        'grayish-purple': '#AF94AE',
+        'pinkish-purple': '#E6CDDF',
+        'dark-purple': '#734A73',
+        'dark-blue-bg': '#202442',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
