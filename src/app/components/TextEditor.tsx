a8bb8525afdbffbd79cec34072f2e08493ac58ec
Lisa <lisacjheinhold@gmail.com>
2024-02-18 13:36:30 -0700

    text editor added to form

---
+++ src/app/components/TextEditor.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import Quill from 'quill';
+
+export default function TextEditor() {
+  const [value, setValue] = useState('');
+  // const quill = new Quill('#editor', {
+  //   modules: {
+  //     toolbar: [
+  //       [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  //       [{ font : [] }],
+  //       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+  //       [{ list: 'ordered' }, { list: 'bullet' }],
+  //       ['image', 'code-block', 'attachments', 'link', 'video'],
+  //     ],
+  //   },
+  //   placeholder: 'Write a note...',
+  //   theme: 'snow', // or 'bubble'
+  // });
+  return (
+
+    <div className='bg-white'>
+    <ReactQuill
+      theme='snow'
+      value={value}
+      onChange={setValue}
+      modules={{
+        toolbar: [
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ font: [] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['image', 'code-block', 'link', 'video'],
+        ],
+      }}
+      placeholder='Write a note...'
+      className='text-slate-600 border border-white rounded-lg p-1'
+    />
+  </div>
+  )
+}
