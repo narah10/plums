b570125dd6eff84782d58811ee6636e7577cc8c6
Lisa <lisacjheinhold@gmail.com>
2024-02-22 13:53:13 -0700

    fav and recent widget

---
+++ src/app/components/Dash-Favorite.tsx
@@ -0,0 +1,14 @@
+export default function DashFavorite(){
+  return(
+    <section className=" bg-gray text-black rounded-lg p-5 m-5 lg:w-1/2 md:w-3/4 w-full">
+        <h1 className="text-2xl">Favorites</h1>
+        <div className="flex flex-col m-3">
+          <ul className="[&>*:nth-child(odd)]:bg-slate-200">
+            <li className="m-2 flex justify-between py-1 px-3">Task 1<span className="text-slate-400">03/18/2018</span></li>
+            <li className="m-2 flex justify-between py-1 px-3">Task 2<span className="text-slate-400">03/18/2018</span></li>
+            <li className="m-2 flex justify-between py-1 px-3">Task 3<span className="text-slate-400">03/18/2018</span></li>
+          </ul>
+        </div>
+    </section>
+  )
+}
